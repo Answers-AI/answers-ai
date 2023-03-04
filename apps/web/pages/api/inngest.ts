a2c21106d269a76a5aee3ae4fd1a11bf38547914
Max Techera <maxi.techerag@gmail.com>
2023-03-03 23:33:44 -0300

    Add embeddings file

--- apps/web/pages/api/inngest.ts
+++ apps/web/pages/api/inngest.ts
@@ -4,6 +4,7 @@ import { createInngestFunctions } from 'utils/dist/ingest/EventVersionHandler';
 import * as jiraFunctions from 'utils/dist/ingest/jira';
 import * as promptFunctions from 'utils/dist/ingest/prompt';
 import * as slackFunctions from 'utils/dist/ingest/slack';
+import * as embeddingsFunctions from 'utils/dist/ingest/embeddings';
 
 // Create a client to send and receive events
 
@@ -12,6 +13,7 @@ export const inngest = new Inngest({ name: 'My App' });
 const functions = Object.values({
   ...jiraFunctions,
   ...slackFunctions,
+  ...embeddingsFunctions,
   ...promptFunctions
 });
 
