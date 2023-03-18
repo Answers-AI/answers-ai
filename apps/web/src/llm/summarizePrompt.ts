77ef502f306729a1f88bf705d10301b43ca0a5eb
Max Techera <maxi.techerag@gmail.com>
2023-03-18 15:37:25 -0300

    Remove agent_scratchpad

--- apps/web/src/llm/summarizePrompt.ts
+++ apps/web/src/llm/summarizePrompt.ts
@@ -14,8 +14,5 @@ Write a summary of what we learn about the story from the following chat log.
 
 {input}
 
-STORY DETAILS:
-
-This was your previous work (but I haven't seen any of it! I only see what you return as final answer):
-{agent_scratchpad}`)
+STORY DETAILS:`)
 ]);
