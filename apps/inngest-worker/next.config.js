da42b08a4e64bda044358543473492c57923075c
Max Techera <maxi.techerag@gmail.com>
2023-05-08 21:22:45 -0300

    Add separate app for inngest workers

---
+++ apps/inngest-worker/next.config.js
@@ -0,0 +1,43 @@
+const { PrismaPlugin } = require('experimental-prisma-webpack-plugin');
+const webpack = require('webpack');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+});
+
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = withBundleAnalyzer({
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ['@prisma/client']
+  },
+  reactStrictMode: true,
+  transpilePackages: ['ui', 'db', 'utils'],
+  modularizeImports: {
+    // '@mui/material/?(((\\w*)?/?)*)': {
+    //   transform: '@mui/material/{{ matches.[1] }}/{{member}}'
+    // },
+    '@mui/icons-material/?(((\\w*)?/?)*)': {
+      transform: '@mui/icons-material/{{ matches.[1] }}/{{member}}'
+    }
+  },
+  webpack: (config, { isServer }) => {
+    config.externals = [...config.externals, 'db', 'puppeteer'];
+
+    config.plugins = [
+      ...config.plugins,
+      // new PrismaPlugin(),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /canvas/,
+        contextRegExp: /jsdom$/
+      })
+    ];
+
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  }
+});
