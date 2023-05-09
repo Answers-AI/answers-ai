f4f780144c28acf376ed89f34a294b4919ea30a5
Max Techera <maxi.techerag@gmail.com>
2023-05-09 11:37:33 -0300

    Add log for healthcheck

--- apps/inngest-worker/pages/api/healthcheck.ts
+++ apps/inngest-worker/pages/api/healthcheck.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const healthCheck = (req: NextApiRequest, res: NextApiResponse) => {
+  console.log('Healthcheck');
   return res.send('ok');
 };
 
