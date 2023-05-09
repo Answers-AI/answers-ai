51cb8ebb0ac5b147eb15032e74d2b46f291ef07c
Max Techera <maxi.techerag@gmail.com>
2023-05-08 21:46:35 -0300

    Update Inngest local endpoint for dev

--- apps/inngest-worker/next.config.js
+++ apps/inngest-worker/next.config.js
@@ -9,7 +9,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
  */
 module.exports = withBundleAnalyzer({
   experimental: {
-    appDir: true,
+    // appDir: true,
     serverComponentsExternalPackages: ['@prisma/client']
   },
   reactStrictMode: true,
