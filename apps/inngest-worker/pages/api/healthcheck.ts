76962e7c7d4e9c971653a1202ea1d703c7386427
Max Techera <maxi.techerag@gmail.com>
2023-05-09 15:05:53 -0300

    Remove console.log

--- apps/inngest-worker/pages/api/healthcheck.ts
+++ apps/inngest-worker/pages/api/healthcheck.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const healthCheck = (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('Healthcheck');
   return res.send('ok');
 };
 
