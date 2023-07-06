9db3e91a9c75187c998bbe6da839cebbe6e407cd
Max Techera <maxi.techerag@gmail.com>
2023-07-06 20:12:31 -0300

    remove db from externals

--- apps/inngest-worker/next.config.js
+++ apps/inngest-worker/next.config.js
@@ -26,7 +26,7 @@ let nextConfig = withBundleAnalyzer({
   },
 
   webpack: (config, { isServer }) => {
-    config.externals = [...config.externals, 'db', 'puppeteer', 'canvas'];
+    config.externals = [...config.externals, 'puppeteer', 'canvas'];
     config.plugins = [
       ...config.plugins,
       // new PrismaPlugin(),
