67b3681b11cee3161e8376e24b925d15b5779f1f
Max Techera <maxi.techerag@gmail.com>
2023-07-07 18:42:53 -0300

    order chats by new, use the correct user image

--- packages/ui/src/ChatLayout/ChatLayout.Server.tsx
+++ packages/ui/src/ChatLayout/ChatLayout.Server.tsx
@@ -48,7 +48,20 @@ export default async function ChatUILayout({
         createdAt: 'desc'
       },
       include: {
-        chats: { include: { prompt: true, messages: { take: 1 } } }
+        chats: {
+          orderBy: {
+            createdAt: 'desc'
+          },
+          include: {
+            prompt: true,
+            messages: {
+              orderBy: {
+                createdAt: 'desc'
+              },
+              take: 1
+            }
+          }
+        }
       }
     })
     .then((data: any) => JSON.parse(JSON.stringify(data)));
