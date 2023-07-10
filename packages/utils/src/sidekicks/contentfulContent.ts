00b45390116a8ed21b8fb7209a3ce6eb0c8b984c
Brad Taylor <bradtaylorsf@gmail.com>
2023-07-10 09:20:58 -0700

    contentful

--- packages/utils/src/sidekicks/contentfulContent.ts
+++ packages/utils/src/sidekicks/contentfulContent.ts
@@ -6,16 +6,17 @@ const sidekick: Sidekick = {
   value: 'contentfulContent',
   placeholder: 'I can help you publish content in Contentful',
   getSystemPromptTemplate: () => {
-    return `You only repate what I say.`;
+    return `You only repeat what I say.`;
   },
   getUserPromptTemplate: (query, context) => {
-    return `Repeat what I say. Repeat only the following and do not say anything else. Do not say repeat. Only show the Header, Body, and Image Concept and link. Print it in plain text:
+    return `Repeat what I say. Repeat only the following and do not say anything else. Do not say repeat. Only show the Header, Sub Headline, Body, and Image Concept and link. Print it in plain text:
 
-    Header:\nLaunching Innovation: The Dynamic Partnership of Contentful and Last Rev\n\n
-    Body:\nExplore the extraordinary collaboration between Contentful and Last Rev as they join forces to revolutionize the digital landscape. Witness their groundbreaking Kickstarter program that combines Contentful's cutting-edge content infrastructure with Last Rev's dynamic creative solutions. Experience the future of digital innovation and discover how this partnership is reshaping the way businesses create, manage, and deliver content like never before.\n
-    Image Concept:\n /imagine Launching Innovation, A futuristic cityscape with towering skyscrapers, showcasing the collaboration between Contentful and Last Rev, with Contentful's infrastructure forming the foundation of the city, and Last Rev's creative solutions bringing it to life, Photography, using a wide-angle lens to capture the grandeur of the city, --ar 16:9 --v 5
-    ![](https://images.ctfassets.net/imglmb3xms7o/4SOvevjHwBzngyC5ITe7oC/49ae570cd5816014ff7e4acb099cab06/Screenshot_2023-06-21_at_9.18.41_PM.png?w=1400&q=100) \n\n
-    [Edit in Contentful](https://app.contentful.com/spaces/imglmb3xms7o/environments/master/entries/4259Oc7JX5tdWKsJbSqQ77)\n\n
+    Header:\nJourneys\n\n
+    Sub Headline: Collaboration Reimagined
+    Body:\nAnswerAI Journeys is not just a feature; it's a transformative approach to tackling complex projects and goals. It's about bringing together minds, breaking down tasks, and navigating through challenges with the power of AI. Experience a journey where collaboration meets innovation, and every step brings you closer to your goal.\n
+    Image Concept:\n /imagine A group of diverse individuals climbing a mountain together, symbolizing collaboration and teamwork. Black background with futuristic landscapes, mountainous vistas, isometric, layers and lines, scientific illustrations, bold and colorful graphic design with neon colors, human connections, --ar 16:9 --v 5
+    ![](https://images.ctfassets.net/e4vn8tcbbhts/39pzE3DzjiftIgALGluyRy/100161be4bf4bd46100fad57870f3c10/Screenshot_2023-07-10_at_8.31.37_AM.png?w=1400&q=100) \n\n
+    [Edit in Contentful](https://app.contentful.com/spaces/e4vn8tcbbhts/entries/4CSmvHUqAu3JQ8QuRFvGhi)\n\n
 
   """
     `;
