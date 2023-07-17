a20f28b1ddd17c49480e78ae5b8f32f83f43f8af
Max Techera <maxi.techerag@gmail.com>
2023-07-17 15:43:08 -0300

    HOTFIX: Add log for worker healthz

--- apps/inngest-worker/pages/api/healthcheck.ts
+++ apps/inngest-worker/pages/api/healthcheck.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const healthCheck = (req: NextApiRequest, res: NextApiResponse) => {
+  console.log('Worker Healthy');
   return res.send('ok');
 };
 
