322cf1915c269d50df258a9602a90d9cdc336728
Adam Harris <adam.a.harris@gmail.com>
2023-07-21 14:07:17 -0700

    Update sentry configs and types not being in dev dep

--- apps/web/sentry.client.config.js
+++ apps/web/sentry.client.config.js
@@ -1,5 +1,7 @@
 import * as Sentry from '@sentry/nextjs';
 
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+
 Sentry.init({
   dsn: 'https://31104059ebc64f998bb81cfb2b1c8814@o4505128356544512.ingest.sentry.io/4505128358510592',
   beforeSend: event => {
@@ -9,7 +11,8 @@ console.error(event);
     }
     return event;
   },
-  tracesSampleRate: 1.0
+  tracesSampleRate: 1.0,
+  enabled: !IS_DEVELOPMENT
   // ...
 
   // Note: if you want to override the automatic release value, do not set a
--- apps/web/sentry.edge.config.js
+++ apps/web/sentry.edge.config.js
@@ -1,11 +1,14 @@
 import * as Sentry from '@sentry/nextjs';
 
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
+
 Sentry.init({
   dsn: 'https://31104059ebc64f998bb81cfb2b1c8814@o4505128356544512.ingest.sentry.io/4505128358510592',
 
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
-  tracesSampleRate: 1.0
+  tracesSampleRate: 1.0,
+  enabled: !IS_DEVELOPMENT
 
   // ...
 
