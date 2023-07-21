3c8285f95df02acc82a734d22bc5c320b67b8554
Adam Harris <adam.a.harris@gmail.com>
2023-07-21 13:49:30 -0700

    Fixed build issue

--- packages/utils/src/utilities/getDomainUrlsFromMarkdown.ts
+++ packages/utils/src/utilities/getDomainUrlsFromMarkdown.ts
@@ -1,16 +1,17 @@
 const getDomainUrlsFromMarkdown = (markdownString: string, origin: string): string[] => {
-    const linkPattern = /\[([^\]]+)\]\(([^)]+)\)/g;
-    const matches = markdownString.matchAll(linkPattern);
-    const filteredUrls: string[] = [];
-  
-    for (const match of matches) {
-      const url = match[2];
-      if (url.startsWith(origin)) {
-        filteredUrls.push(url);
-      }
+  const linkPattern = /\[([^\]]+)\]\(([^)]+)\)/g;
+  const matches = Array.from(markdownString.matchAll(linkPattern));
+  const filteredUrls: string[] = [];
+
+  for (let i = 0; i < matches.length; i++) {
+    const match = matches[i];
+    const url = match[2];
+    if (url.startsWith(origin)) {
+      filteredUrls.push(url);
     }
-  
-    return filteredUrls;
   }
 
-  export default getDomainUrlsFromMarkdown;
+  return filteredUrls;
+}
+
+export default getDomainUrlsFromMarkdown;
