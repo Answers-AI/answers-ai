9aca5f52e67f14842904d2bb5f81cc5284df6df9
Max Techera <maxi.techerag@gmail.com>
2023-07-28 13:24:42 -0300

    HOTFIX: sidekicks not loading on journey

--- apps/web/app/(Main UI)/(Chat UI)/journey/[journeyId]/page.tsx
+++ apps/web/app/(Main UI)/(Chat UI)/journey/[journeyId]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { prisma } from '@db/client';
 import Chat from '@ui/Chat';
+import getCachedSession from '@ui/getCachedSession';
 
 export const metadata = {
   title: 'Journey | Answers AI',
@@ -8,7 +9,9 @@ export const metadata = {
 };
 
 const JourneyDetailPage = async ({ params }: any) => {
-  const journey = await prisma.journey
+  const session = await getCachedSession();
+
+  const journeyPromise = prisma.journey
     .findUnique({
       where: {
         id: params.journeyId
@@ -17,8 +20,25 @@ const JourneyDetailPage = async ({ params }: any) => {
     })
     .then((data: any) => JSON.parse(JSON.stringify(data)));
 
+  const sidekicksPromise = prisma.sidekick.findMany({
+    where: {
+      OR: [
+        {
+          favoritedBy: {
+            some: {
+              id: session?.user?.id!
+            }
+          }
+        },
+        {
+          isSystem: true
+        }
+      ]
+    }
+  });
+  const [journey, sidekicks] = await Promise.all([journeyPromise, sidekicksPromise]);
   // @ts-expect-error Async Server Component
-  return <Chat {...params} journey={journey} />;
+  return <Chat {...params} journey={journey} sidekicks={sidekicks} />;
 };
 
 export default JourneyDetailPage;
