98539358488620007b1cb3a47a97313033138124
Max Techera <maxi.techerag@gmail.com>
2024-03-18 14:18:09 -0300

    Remove console.log statements from middleware and auth0 files

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -8,14 +8,7 @@ import {
   initAuth0 // note the edge runtime specific `initAuth0`
 } from '@auth0/nextjs-auth0/edge';
 import { NextResponse } from 'next/server';
-console.log('AUTH-edge', {
-  secret: process.env.AUTH0_SECRET,
-  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-  baseURL: process.env.AUTH0_BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET,
-  idTokenSigningAlg: process.env.AUTH0_TOKEN_SIGN_ALG
-});
+
 const auth0 = initAuth0({
   secret: process.env.AUTH0_SECRET,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
