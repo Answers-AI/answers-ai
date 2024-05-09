755a4771962b68932e1d7ae86cfe1a247a8bedb4
Max Techera <maxi.techerag@gmail.com>
2024-05-09 13:25:28 -0300

    HOTFIX: condition

--- packages/utils/src/findSidekicksForChat.ts
+++ packages/utils/src/findSidekicksForChat.ts
@@ -5,30 +5,21 @@ import { User } from 'types';
 export async function findSidekicksForChat(user: User) {
   const dbSidekicks = await prisma.sidekick.findMany({
     where: {
-      AND: [
-        { tags: { has: 'flowise' } },
-        { NOT: { tags: { has: 'internal' } } },
+      tags: { has: 'flowise' },
+      NOT: { tags: { has: 'internal' } },
+      chatflow: {
+        path: ['answersConfig', 'workflowVisibility'],
+        array_contains: ['AnswerAI']
+      },
+      OR: [
+        { createdByUser: { id: user.id } },
         {
+          organization: { id: user.org_id },
           chatflow: {
             path: ['answersConfig', 'workflowVisibility'],
-            array_contains: ['AnswerAI']
+            array_contains: ['Organzation']
           }
         },
-        {
-          OR: [
-            { createdByUser: { id: user.id } },
-            {
-              organization: { id: user.org_id },
-              chatflow: {
-                path: ['answersConfig', 'workflowVisibility'],
-                array_contains: ['Organzation']
-              }
-            },
-            {
-              isSystem: true
-            }
-          ]
-        }
       ]
     }
   });
